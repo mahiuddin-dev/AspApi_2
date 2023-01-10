30ddf1b09f116619203baf86477690eb70072860
mahiuddin-dev <mahiuddin@mahiuddin>
2023-01-10 05:09:43 +0000

    add post method and get method

---
+++ AspApi/Controllers/PostController.cs
@@ -0,0 +1,43 @@
+﻿using AspApi.Context;
+using AspApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace AspApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        ApplicationDBContext _dbContext;
+
+        public PostController(ApplicationDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public List<Post> Getall()
+        {
+            var posts = _dbContext.Posts.ToList();
+            return posts;
+        }
+
+        [HttpPost]
+        public Post AddPost(Post post)
+        {
+            post.CreateDate = DateTime.Now;
+            _dbContext.Posts.Add(post);
+
+            bool isSaved = _dbContext.SaveChanges() > 0;
+
+            if(isSaved)
+            {
+                return post;
+            }
+            return null;
+        }
+
+    }
+}
